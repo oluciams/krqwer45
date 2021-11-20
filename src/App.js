3b2fba4bee70d5d4735656084e1f6aa84017fa87
olga <oluciams84@gmail.com>
2021-11-20 17:58:59 -0500

    Agrega onClick para al il para actualizar la clase

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ const App = ()=>{
         console.log(newName) 
       }
 
+      const handleOnClick= (e)=>{
+        const id = e.target.id
+        const position= id-1
+        let copyTasks = tasks
+        // console.log( copyTasks[position])   
+        copyTasks[position].done = !copyTasks[position].done
+        // console.log( copyTasks[position])
+        setTasks([...copyTasks])
+      }
+
       const handleOnSubmit = (e)=>{
         e.preventDefault()
         if( inputName === ''){
@@ -35,14 +45,13 @@ const App = ()=>{
           setInputName('')              
         }
       }   
-      // newTask: '' 
-  
+        
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {tasks.map((task, index) => <li className={task.done ? 'done' : ''} key={task.id}>{task.name}</li>)}
+            {tasks.map((task, index) => <li id={task.id} onClick={handleOnClick} className={task.done ? 'done' : ''} key={task.id}>{task.name}</li>)}
           </ul>
           <form onSubmit={handleOnSubmit}>
             <input className={error ? 'error' : ''} type="text" value={inputName} onChange={handleinputName} id="new-task" placeholder="Ingresa una tarea y oprime Enter"/>
@@ -55,33 +64,3 @@ const App = ()=>{
 
 export default App;
 
-// class App extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       tasks: [
-//         { id: 1, name: "Sacar la ropa", done: false },
-//         { id: 2, name: "Hacer la cama", done: true },
-//         { id: 3, name: "Leer un rato", done: false }
-//       ],
-//       newTask: ''
-//     }
-//   }
-//   render() {
-//     return (
-//       <div className="wrapper">
-//         <div className="list">
-//           <h3>Por hacer:</h3>
-//           <ul className="todo">
-//             {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
-//           </ul>
-//           <form>
-//             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
-//           </form>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App;
